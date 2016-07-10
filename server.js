aa553fd5b94e7247b69c1e2918b8e3afb12917de
EddieWen-Taiwan <m20021991y@gmail.com>
2016-07-10 17:48:45 +0800

    Console log public link

--- server.js
+++ server.js
@@ -44,5 +44,16 @@ app.get('/demo', function(req, res) {
 const port = 8080;
 
 app.listen( port, function() {
-	console.log(`Example app listening on port http://localhost:${port}!`);
+	console.log(`Example app listening on port ${port}`);
+	console.log(`Public visit from             http://${getPublicAddress()}:${port}\n`);
 });
+
+
+
+function getPublicAddress() {
+	let address, ifaces=require('os').networkInterfaces();
+	for(let dev in ifaces){
+		ifaces[dev].filter((details)=>details.family==='IPv4'&&details.internal===false?address=details.address:undefined);
+	}
+	return address;
+}
