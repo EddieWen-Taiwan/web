99165ec4a462bc6700d53c1cc8cf7f62981d34d0
EddieWen-Taiwan <m20021991y@gmail.com>
2016-07-10 18:07:46 +0800

    Use `node-ip` to get my ip address

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const ip = require('ip');
 /**
  * Include webpack....
  */
@@ -45,15 +46,5 @@ const port = 8080;
 
 app.listen( port, function() {
 	console.log(`Example app listening on port ${port}`);
-	console.log(`Public visit from             http://${getPublicAddress()}:${port}\n`);
+	console.log(`Public visit from             http://${ip.address()}:${port}\n`);
 });
-
-
-
-function getPublicAddress() {
-	let address, ifaces=require('os').networkInterfaces();
-	for(let dev in ifaces){
-		ifaces[dev].filter((details)=>details.family==='IPv4'&&details.internal===false?address=details.address:undefined);
-	}
-	return address;
-}
