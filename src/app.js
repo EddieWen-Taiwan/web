0307fb4502169698f8c03370750473f6dcd5c3b2
Eddie Wen <eddiewen.tw@gmail.com>
2018-03-03 17:17:56 +0800

    chore: delete commented codes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,2 @@
-/* eslint-disable */
-// const req = require.context('./css', true, /^(.*\.(css$))[^.]*$/igm);
-// req.keys().forEach((key)=>{req(key);});
-/* eslint-enable */
 import './css/reset.css';
 import './css/main.css';
